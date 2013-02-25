bb4f2bcf74c5ea29c0f07ed70f23e9404bf9aaa1
Peter Gribanov <wcode404@gmail.com>
2013-02-25 23:20:32 +0400

    repair link to autofill

--- src/guepard/templates/homedb/js/init.js
+++ src/guepard/templates/homedb/js/init.js
@@ -1,8 +1,8 @@
 $(function(){
 
 $('#catalog-item-autofill').click(function(e){
-//	$(this).parents('form').attr({action:'/anime/autofill.html'});
-	location.href = '/anime/autofill.html';
+//	$(this).parents('form').attr({action:'/item-autofill.html'});
+	location.href = '/item-autofill.html';
 	return false;
 });
 $('input:submit, button, .catalog-last-added .details').button();
