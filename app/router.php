d9cba41c96561e1244429858bbd236a6384c25aa
peter-gribanov <wcode404@gmail.com>
2013-04-11 12:01:52 +0400

    correct route to prod

--- app/router.php
+++ app/router.php
@@ -1,25 +1,42 @@
 <?php
+/**
+ * AnimeDB package
+ *
+ * @package   AnimeDB
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
+
 
 // run not in cli-server
 if (PHP_SAPI != 'cli-server') {
 	exit('This script can be run from the CLI-server only.');
 }
 
+
+// get request
+require_once __DIR__.'/bootstrap.php.cache';
+$request = Request::createFromGlobals();
+
 // Check that the access to the application by the local computer or local network
 // Comment this code to open access to the application from the internet
-if (!empty($_SERVER['HTTP_CLIENT_IP']) ||
-	!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ||
-	empty($_SERVER['REMOTE_ADDR']) ||
+if ($request->server->get('HTTP_CLIENT_IP') ||
+	$request->server->get('HTTP_X_FORWARDED_FOR') ||
+	!($addr = $request->server->get('REMOTE_ADDR')) ||
 	( // localhost
-		!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) &&
+		!in_array($addr, array('127.0.0.1', 'fe80::1', '::1')) &&
 		(
 			( // local network IPv6
-				$ipv6 = (strpos($_SERVER['REMOTE_ADDR'], ':') !== false) &&
-				strpos($_SERVER['REMOTE_ADDR'], 'fc00::') !== 0
+				($ipv6 = (strpos($addr, ':') !== false)) &&
+				strpos($addr, 'fc00::') !== 0
 			) ||
 			( // local network IPv4
 				!$ipv6 &&
-				($long = ip2long($_SERVER['REMOTE_ADDR'])) === false ||
+				($long = ip2long($addr)) === false ||
 				!(
 					($long >= ip2long('10.0.0.0')    && $long <= ip2long('10.255.255.255')) ||
 					($long >= ip2long('172.16.0.0')  && $long <= ip2long('172.31.255.255')) ||
@@ -33,34 +50,25 @@ if (!empty($_SERVER['HTTP_CLIENT_IP']) ||
 	exit('You are not allowed to access this application.');
 }
 
-return false;
-
-/*
-$call_not_found = function () {
-	require 'guepard/404.php';
-	exit(0);
-};
-
-if (!($path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
-	$call_not_found();
-}
-
-$path .= $path[strlen($path)-1] == '/' ? 'index.php' : '';
-
-// give static
-if (file_exists(__DIR__.$path)) {
+// give static or run for dev
+if (is_file(__DIR__.'/../web'.parse_url($request->getRequestUri(), PHP_URL_PATH)) || $request->getScriptName() == '/app_dev.php') {
 	return false;
 }
 
 
-// route if need
-
-#RewriteRule [0-9]+\-[a-z]+\.html		item.php		[QSA,L] # действие над элементом
-#RewriteRule [0-9]+\.html				item.php		[QSA,L] # просмотр элемента
-#RewriteRule item\-[a-z]+\.html			item.php		[QSA,L] # действие над новым элементом
-if (preg_match('/^\/(?:\d+(?:\-[a-z]+)?)|(?:item\-[a-z]+)\.html$/', $path)) {
-	return require 'item.php';
-}
-
-$call_not_found();
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+/*
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
 */
+
+require_once __DIR__.'/AppKernel.php';
+require_once __DIR__.'/AppCache.php';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+$kernel = new AppCache($kernel);
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
