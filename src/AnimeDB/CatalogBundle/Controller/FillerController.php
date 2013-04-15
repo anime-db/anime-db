333d3d973a38bbcea9c084ff0823a87926b6b62a
Peter Gribanov <info@peter-gribanov.ru>
2013-04-15 12:09:23 +0400

    add controller for fill item before add

---
+++ src/AnimeDB/CatalogBundle/Controller/FillerController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * AnimeDB package
+ *
+ * @package   AnimeDB
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDB\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Контроллер наполнителя записи
+ *
+ * @package AnimeDB\CatalogBundle\Controller
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class FillerController extends Controller
+{
+}
