bbb30a11636ea85af465cc609053e6aad1aeb680
Peter Gribanov <info@peter-gribanov.ru>
2013-04-18 16:01:03 +0400

    add defaults form for simple entitys

---
+++ src/AnimeDB/CatalogBundle/Form/ImageType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * AnimeDB package
+ *
+ * @package   AnimeDB
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDB\CatalogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Image form
+ *
+ * @package AnimeDB\CatalogBundle\Form
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class ImageType extends AbstractType
+{
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Form.AbstractType::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('source');
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Form.AbstractType::setDefaultOptions()
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AnimeDB\CatalogBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Symfony\Component\Form.FormTypeInterface::getName()
+     */
+    public function getName()
+    {
+        return 'animedb_catalogbundle_imagetype';
+    }
+}
