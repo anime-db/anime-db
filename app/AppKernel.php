68afaec0f4e1297cf97f105dad5d2ff62becaeeb
Peter Gribanov <info@peter-gribanov.ru>
2013-05-16 17:22:29 +0400

    remove genemu

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new AnimeDB\CatalogBundle\AnimeDBCatalogBundle(),
-            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
