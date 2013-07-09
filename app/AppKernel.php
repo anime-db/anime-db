ca6a8215a1cd727d17d45b76e0a00a2ea556ed1f
Peter Gribanov <info@peter-gribanov.ru>
2013-07-09 12:27:38 +0400

    add AvalancheImagineBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new AnimeDB\CatalogBundle\AnimeDBCatalogBundle(),
+            new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
