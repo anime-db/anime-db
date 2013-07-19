6f1ecaa366eee6435d0837d71cee9d713ad5f228
Peter Gribanov <info@peter-gribanov.ru>
2013-07-19 16:58:17 +0400

    add method for get folders for path

--- src/AnimeDB/CatalogBundle/Controller/FormController.php
+++ src/AnimeDB/CatalogBundle/Controller/FormController.php
@@ -12,6 +12,7 @@ namespace AnimeDB\CatalogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Form
@@ -21,6 +22,47 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class FormController extends Controller
 {
+    /**
+     * Return list folders for directory
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function foldersAction(Request $request)
+    {
+        $path = realpath($request->get('path', __DIR__.'/../../../../'));
+        if (!$path || !is_dir($path) || !is_readable($path)) {
+            throw $this->createNotFoundException('Cen\'t read directory: '.$path);
+        }
+        // add slash if need
+        $path .= $path[strlen($path)-1] != DIRECTORY_SEPARATOR ? DIRECTORY_SEPARATOR : '';
+        $show_hidden = (int)$request->get('show_hidden', 0);
+
+        // scan directory
+        $d = dir($path);
+        $folders = [];
+        while (false !== ($entry = $d->read())) {
+            if ($entry == '.') {
+                continue;
+            }
+            $realpath = realpath($path.$entry.DIRECTORY_SEPARATOR);
+            // if read path is root path then parent path is also equal to root
+            if ($realpath && $realpath != $path && is_dir($realpath) && is_readable($realpath)) {
+                if ($entry == '..' || $entry[0] != '.' || $show_hidden) {
+                    $folders[$entry] = [
+                        'name' => $entry,
+                        'path' => $realpath.DIRECTORY_SEPARATOR
+                    ];
+                }
+            }
+        }
+        $d->close();
+        ksort($folders);
+
+        return new JsonResponse(['folders' => array_values($folders)]);
+    }
+
     /**
      * Rand and return template
      *
