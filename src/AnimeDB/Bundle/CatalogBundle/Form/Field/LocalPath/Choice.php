2259ec27a925b6cd0afd18354e351996337f3524
Peter Gribanov <info@peter-gribanov.ru>
2013-07-26 10:21:00 +0400

    add form for form field local path in pop-up

---
+++ src/AnimeDB/Bundle/CatalogBundle/Form/Field/LocalPath/Choice.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * AnimeDB package
+ *
+ * @package   AnimeDB
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDB\Bundle\CatalogBundle\Form\Field\LocalPath;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Local path choice form
+ *
+ * @package AnimeDB\Bundle\CatalogBundle\Form\Field\LocalPath
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class Choice extends AbstractType
+{
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Form\AbstractType::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('path', 'text', [
+            'label' => 'Path',
+            'required' => true
+        ]);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Symfony\Component\Form\FormTypeInterface::getName()
+     */
+    public function getName()
+    {
+        return 'local_path_popup';
+    }
+}
