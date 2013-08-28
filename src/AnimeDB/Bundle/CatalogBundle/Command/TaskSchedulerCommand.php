307c594f87175b260a753fb6ca64f17ea8bd819d
Peter Gribanov <info@peter-gribanov.ru>
2013-08-28 12:00:59 +0400

    add TaskScheduler

--- src/AnimeDB/Bundle/CatalogBundle/Command/TaskSchedulerCommand.php
+++ src/AnimeDB/Bundle/CatalogBundle/Command/TaskSchedulerCommand.php
@@ -13,6 +13,8 @@ namespace AnimeDB\Bundle\CatalogBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use AnimeDB\Bundle\CatalogBundle\Entity\Task;
+use Symfony\Component\Process\PhpExecutableFinder;
 
 /**
  * Task Scheduler
@@ -22,6 +24,13 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class TaskSchedulerCommand extends ContainerAwareCommand
 {
+    /**
+     * Maximum sleep time
+     *
+     * @var integer
+     */
+    const MAX_SLEEP_TIME = 86400;
+
     /**
      * (non-PHPdoc)
      * @see Symfony\Component\Console\Command.Command::configure()
@@ -41,9 +50,74 @@ class TaskSchedulerCommand extends ContainerAwareCommand
         if (!$this->getContainer()->getParameter('task-scheduler')['enabled']) {
             return null;
         }
+
+        // path to php executable
+        $finder = new PhpExecutableFinder();
+        $php = $finder->find();
+
+        $output->writeln('Task Scheduler');
+
         while (true) {
-            // TODO get tasks and run this
-            sleep(86400);
+            $task = $this->getNextTask();
+
+            // task is exists
+            if ($task instanceof Task) {
+                $output->writeln('Run <info>'.$task->getCommand().'</info>');
+                exec($php.' '.__DIR__.'/../../../../../app/console '.$task->getCommand().' >/dev/null 2>&1 &');
+            }
+
+            // fall asleep waiting for next task
+            $time = $this->getSleepTime();
+            $output->writeln('Sleep <comment>'.$time.'</comment> s.');
+            sleep($time);
         }
+    }
+
+    /**
+     * Get next task
+     *
+     * @return \AnimeDB\Bundle\CatalogBundle\Entity\Task|null
+     */
+    protected function getNextTask()
+    {
+        $repository = $this->getContainer()->get('doctrine')
+            ->getRepository('AnimeDBCatalogBundle:Task');
+
+        return $repository->createQueryBuilder('t')
+                ->where('t.status = :status AND t.next_run <= :time')
+                ->setParameter('status', Task::STATUS_ENABLED)
+                ->setParameter('time', date('Y-m-d H:m:s'))
+                ->orderBy('t.next_run', 'ASC')
+                ->getQuery()
+                ->getOneOrNullResult();
+    }
+
+    /**
+     * Get sleep time
+     *
+     * @return integer
+     */
+    protected function getSleepTime()
+    {
+        $current = time();
+        $time = self::MAX_SLEEP_TIME;
+        $repository = $this->getContainer()->get('doctrine')
+            ->getRepository('AnimeDBCatalogBundle:Task');
+
+        // get next task
+        $task = $repository->createQueryBuilder('t')
+            ->where('t.status = :status AND t.next_run > :time')
+            ->setParameter('status', Task::STATUS_ENABLED)
+            ->setParameter('time', date('Y-m-d H:m:s', $current))
+            ->orderBy('t.next_run', 'ASC')
+            ->getQuery()
+            ->getOneOrNullResult();
+
+        // task is exists
+        if ($task instanceof Task) {
+            $time = $task->getNextRun()->getTimestamp() - $current;
+        }
+
+        return min($time, self::MAX_SLEEP_TIME);
     }
 }
