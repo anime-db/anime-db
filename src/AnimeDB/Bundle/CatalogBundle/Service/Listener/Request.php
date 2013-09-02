2871a5ec40c7113370c5cbede12344f91c6aacd4
Peter Gribanov <info@peter-gribanov.ru>
2013-09-02 10:00:13 +0400

    add methods for get and set locale. validate available languages from request for set correct locale

--- src/AnimeDB/Bundle/CatalogBundle/Service/Listener/Request.php
+++ src/AnimeDB/Bundle/CatalogBundle/Service/Listener/Request.php
@@ -13,6 +13,9 @@ namespace AnimeDB\Bundle\CatalogBundle\Service\Listener;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Gedmo\Translatable\TranslatableListener;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
+use Symfony\Component\Validator\Validator;
+use Symfony\Component\Validator\Constraints\Locale;
 
 /**
  * Request listener
@@ -37,13 +40,22 @@ class Request
     protected $translatable;
 
     /**
+     * Validator
+     *
+     * @var \Symfony\Component\Validator\Validator
+     */
+    private $validator;
+
+    /**
      * Construct
      *
      * @param \Gedmo\Translatable\TranslatableListener $translatable
+     * @param \Symfony\Component\Validator\Validator $validator
      */
-    public function __construct(TranslatableListener $translatable)
+    public function __construct(TranslatableListener $translatable, Validator $validator)
     {
         $this->translatable = $translatable;
+        $this->validator = $validator;
     }
 
     /**
@@ -57,31 +69,57 @@ class Request
         $request = $event->getRequest();
 
         // set default locale from request
-        if ($locale = $request->getPreferredLanguage()) {
-            $request->setDefaultLocale($locale);
+        if ($request_locale = $request->getPreferredLanguage()) {
+            $request->setDefaultLocale($request_locale);
         }
 
-        setlocale(LC_ALL, $request->getLocale());
+        // reset locale
+        $this->setLocale($request, $this->getLocale($request));
+    }
 
-        // set locale from request attribute
-        /* if ($locale = $request->attributes->get(self::SESSION_LOCALE)) {
-            $request->setLocale($locale);
-        } */
-
+    /**
+     * Set current locale
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param string $locale
+     */
+    public function setLocale(HttpRequest $request, $locale)
+    {
         // set locale from session
-        /* if ($request->hasPreviousSession()) {
-            if ($locale = $request->getSession()->get(self::SESSION_LOCALE)) {
-                $request->setLocale($locale);
-            } else {
-                $request->getSession()->set(self::SESSION_LOCALE, $request->getLocale());
-            }
-        } */
-
-        // set translatable language from locale
-        $language = $request->getLocale();
-        if (false !== ($position = strpos($language, '_'))) {
-            $language = substr($language, 0, $position);
+        if ($request->hasPreviousSession()) {
+            $request->getSession()->set(self::SESSION_LOCALE, $locale);
         }
-        $this->translatable->setTranslatableLocale($language);
+        $request->setLocale($locale);
+        setlocale(LC_ALL, $locale);
+        // set translatable
+        $this->translatable->setTranslatableLocale($locale);
+    }
+
+    /**
+     * Get current locale
+     * 
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return string
+     */
+    public function getLocale(HttpRequest $request)
+    {
+        // set locale from session
+        if ($request->hasPreviousSession()) {
+            if ($locale = $request->getSession()->get(self::SESSION_LOCALE)) {
+                return $locale;
+            }
+        }
+
+        // get locale from language list
+        $locale_constraint = new Locale();
+        foreach ($request->getLanguages() as $language) {
+            if (!count($this->validator->validateValue($language, $locale_constraint))) {
+                return $language;
+            }
+        }
+
+        // get default locale
+        return $request->getLocale();
     }
 }
