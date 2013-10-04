89f566185b63a467c8fb998e083911089d7865cc
Peter Gribanov <info@peter-gribanov.ru>
2013-10-04 11:29:34 +0400

    filter storages list from types

--- src/AnimeDb/Bundle/CatalogBundle/Repository/Storage.php
+++ src/AnimeDb/Bundle/CatalogBundle/Repository/Storage.php
@@ -11,6 +11,7 @@
 namespace AnimeDb\Bundle\CatalogBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use AnimeDb\Bundle\CatalogBundle\Entity\Storage as StorageEntity;
 
 /**
  * Storage repository
@@ -21,19 +22,36 @@ use Doctrine\ORM\EntityRepository;
 class Storage extends EntityRepository
 {
     /**
-     * Get storage list
+     * Get storage list for types
+     *
+     * @param array $types
      *
      * @return array [\AnimeDb\Bundle\CatalogBundle\Entity\Storage]
      */
-    public function getList()
+    public function getList(array $types = array())
     {
+        if (!$types || $types == StorageEntity::$type_names) {
+            return $this->getEntityManager()->createQuery('
+                SELECT
+                    s
+                FROM
+                    AnimeDbCatalogBundle:Storage s
+                ORDER BY
+                    s.id DESC
+            ')->getResult();
+        }
+
         return $this->getEntityManager()->createQuery('
             SELECT
                 s
             FROM
                 AnimeDbCatalogBundle:Storage s
+            WHERE
+                s.type IN (:types)
             ORDER BY
                 s.id DESC
-        ')->getResult();
+        ')
+            ->setParameter(':types', $types)
+            ->getResult();
     }
 }
