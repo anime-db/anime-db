38abcf9c5acbdc5358cb97398ad10b86f64de0ae
Peter Gribanov <info@peter-gribanov.ru>
2013-10-11 17:23:30 +0400

    add empty plugin type Assets

---
+++ src/AnimeDb/Bundle/CatalogBundle/Plugin/Assets/Assets.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\CatalogBundle\Plugin\Assets;
+
+use AnimeDb\Bundle\CatalogBundle\Plugin\Plugin;
+use Knp\Menu\ItemInterface;
+
+/**
+ * Plugin for build assets
+ * 
+ * @package AnimeDb\Bundle\CatalogBundle\Plugin\Assets
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+abstract class Assets extends Plugin
+{
+}
