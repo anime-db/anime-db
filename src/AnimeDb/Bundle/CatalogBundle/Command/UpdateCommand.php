01fb2f3fba4b47771002620eb4f69d65e225c04c
Peter Gribanov <info@peter-gribanov.ru>
2013-10-25 09:20:09 +0400

    use github api for find new versions and download it by composer api

--- src/AnimeDb/Bundle/CatalogBundle/Command/UpdateCommand.php
+++ src/AnimeDb/Bundle/CatalogBundle/Command/UpdateCommand.php
@@ -10,13 +10,18 @@
 
 namespace AnimeDb\Bundle\CatalogBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Guzzle\Http\Client;
 use Composer\Factory;
 use Composer\IO\ConsoleIO;
-use Composer\Installer;
+use Composer\Package\Package;
+use AnimeDb\Bundle\CatalogBundle\Event\Update\StoreEvents;
+use AnimeDb\Bundle\CatalogBundle\Event\Update\ApplicationDownloaded;
+use AnimeDb\Bundle\CatalogBundle\Event\Update\ApplicationUpdated;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Update Application
@@ -24,7 +29,7 @@ use Composer\Installer;
  * @package AnimeDb\Bundle\CatalogBundle\Command
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-class UpdateCommand extends Command
+class UpdateCommand extends ContainerAwareCommand
 {
     /**
      * (non-PHPdoc)
@@ -33,10 +38,7 @@ class UpdateCommand extends Command
     protected function configure()
     {
         $this->setName('animedb:update')
-            ->setDescription('Update application and plugins')
-            ->setDefinition([
-                new InputArgument('packages', InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Packages that should be updated, if not provided all packages are.')
-            ]);
+            ->setDescription('Update application');
     }
 
     /**
@@ -44,12 +46,74 @@ class UpdateCommand extends Command
      * @see Symfony\Component\Console\Command.Command::execute()
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
+        // load composer
         $io = new ConsoleIO($input, $output, $this->getHelperSet());
-        $f = new Factory();
-        $composer = $f->createComposer($io);
-        $install = Installer::create($io, $composer)
-            ->setUpdate(true)
-            ->setUpdateWhitelist($input->getArgument('packages'));
-        return $install->run() ? 0 : 1;
+        $factory = new Factory();
+        $composer = $factory->createComposer($io);
+
+        // search tag with new version of application
+        $tag = $this->findNewVersion($composer->getPackage()->getPrettyVersion());
+        if (!$tag) {
+            $output->writeln('Application has already been updated to the latest version');
+            return;
+        }
+        $output->writeln('Discovered a new version of the application: <info>'.$tag['version'].'</info>');
+
+        // create install package
+        $package = new Package('anime-db/anime-db', $tag['version'].'.0', $tag['version']);
+        $package->setDistType('zip');
+        $package->setDistUrl($tag['zipball_url']);
+        $package->setInstallationSource('dist');
+
+        // download new version
+        $dm = $composer->getDownloadManager();
+        $downloader = $dm->getDownloaderForInstalledPackage($package);
+        $target = sys_get_temp_dir().'/anime-db';
+        $downloader->download($package, $target);
+
+        $dispatcher = $this->getContainer()->get('event_dispatcher');
+        // notify about downloaded
+        $dispatcher->dispatch(StoreEvents::APPLICATION_DOWNLOADED, new ApplicationDownloaded($target, $tag['version']));
+
+        // rewriting the application files
+        // TODO do rewriting
+
+        // notify about updated
+        $dispatcher->dispatch(StoreEvents::APPLICATION_UPDATED, new ApplicationUpdated($target, $tag['version']));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getListTags()
+    {
+        $client = new Client('https://api.github.com/');
+        /* @var $response \Guzzle\Http\Message\Response */
+        $response = $client->get('repos/anime-db/anime-db/tags')->send();
+        return json_decode($response->getBody(true), true);
+    }
+
+    /**
+     * Find new version
+     *
+     * @param string $current_version
+     *
+     * @return array|boolean
+     */
+    protected function findNewVersion($current_version)
+    {
+        // search tag with new version of application
+        foreach ($this->getListTags() as $tag) {
+            if (preg_match('/^v?(?<version>\d+\.\d+\.\d+)$/', $tag['name'], $mat)) {
+                if (version_compare($mat['version'], $current_version) == 1) {
+                    return array_merge(['version' => $mat['version']], $tag);
+                } else {
+                    // TODO it is only for dev
+                    return array_merge(['version' => $mat['version']], $tag);
+                    break;
+                }
+            }
+        }
+        return false;
     }
 }
