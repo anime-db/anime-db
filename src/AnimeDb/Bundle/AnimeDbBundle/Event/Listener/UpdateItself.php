bb41283d9d9b4bfc18d144ae99b308c91c919f31
Peter Gribanov <info@peter-gribanov.ru>
2013-10-28 15:29:54 +0400

    after download app update composer requirements

--- src/AnimeDb/Bundle/AnimeDbBundle/Event/Listener/UpdateItself.php
+++ src/AnimeDb/Bundle/AnimeDbBundle/Event/Listener/UpdateItself.php
@@ -21,11 +21,20 @@ use AnimeDb\Bundle\AnimeDbBundle\Event\UpdateItself\Downloaded;
 class UpdateItself
 {
     /**
-     * Update itself hendler
+     * Add requirements in composer.json from old version
      *
      * @param \AnimeDb\Bundle\AnimeDbBundle\Event\UpdateItself\Downloaded $event
      */
-    public function onAppDownloaded(Downloaded $event)
+    public function onAppDownloadedAddComposerRequirements(Downloaded $event)
     {
+        $old_config = file_get_contents(__DIR__.'/../../../../../../composer.json');
+        $old_config = json_decode($old_config, true);
+
+        $new_config = file_get_contents($event->getPath().'/composer.json');
+        $new_config = json_decode($new_config, true);
+
+        $new_config['require'] = array_merge($old_config['require'], $new_config['require']);
+        $new_config = json_encode($new_config, JSON_NUMERIC_CHECK|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+        file_put_contents($event->getPath().'/composer.json', $new_config);
     }
 }
