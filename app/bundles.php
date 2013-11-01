188b8b0651b473a4c90f29a0bef358e9b2a95fab
Peter Gribanov <info@peter-gribanov.ru>
2013-11-01 10:14:29 +0400

    add empty AppBundle

--- app/bundles.php
+++ app/bundles.php
@@ -2,6 +2,7 @@
 return [
     new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
     new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+    new AnimeDb\Bundle\AppBundle\AnimeDbAppBundle(),
     new AnimeDb\Bundle\CatalogBundle\AnimeDbCatalogBundle(),
     new AnimeDb\Bundle\WorldArtFillerBundle\AnimeDbWorldArtFillerBundle()
 ];
