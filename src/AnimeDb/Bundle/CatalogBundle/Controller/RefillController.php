22752c3e77c411eab98adfe898c78fc1641edc04
Peter Gribanov <info@peter-gribanov.ru>
2013-11-25 12:23:45 +0400

    registr the refill forms

--- src/AnimeDb/Bundle/CatalogBundle/Controller/RefillController.php
+++ src/AnimeDb/Bundle/CatalogBundle/Controller/RefillController.php
@@ -13,10 +13,19 @@ namespace AnimeDb\Bundle\CatalogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AnimeDb\Bundle\CatalogBundle\Entity\Item;
 use AnimeDb\Bundle\CatalogBundle\Plugin\Fill\Refiller\Refiller;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\DateEnd as DateEndForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\DateStart as DateStartForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Duration as DurationForm;
 use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Episodes as EpisodesForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\EpisodesNumber as EpisodesNumberForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\FileInfo as FileInfoForm;
 use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Gengres as GengresForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Images as ImagesForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Manufacturer as ManufacturerForm;
 use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Names as NamesForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Sources as SourcesForm;
 use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Summary as SummaryForm;
+use AnimeDb\Bundle\CatalogBundle\Form\Plugin\Refiller\Translate as TranslateForm;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -135,21 +144,57 @@ class RefillController extends Controller
     protected function getForm($field, Item $item_origin, Item $item_fill)
     {
         switch ($field) {
+            case Refiller::FIELD_DATE_END:
+                $form = new DateEndForm();
+                $data = ['date_end' => $item_fill->getDateEnd()];
+                break;
+            case Refiller::FIELD_DATE_START:
+                $form = new DateStartForm();
+                $data = ['date_start' => $item_fill->getDateStart()];
+                break;
+            case Refiller::FIELD_DURATION:
+                $form = new DurationForm();
+                $data = ['duration' => $item_fill->getDuration()];
+                break;
             case Refiller::FIELD_EPISODES:
                 $form = new EpisodesForm();
                 $data = ['episodes' => $item_fill->getEpisodes()];
+                break;
+            case Refiller::FIELD_EPISODES_NUMBER:
+                $form = new EpisodesNumberForm();
+                $data = ['episodes_number' => $item_fill->getEpisodesNumber()];
+                break;
+            case Refiller::FIELD_FILE_INFO:
+                $form = new FileInfoForm();
+                $data = ['file_info' => $item_fill->getFileInfo()];
                 break;
             case Refiller::FIELD_GENRES:
                 $form = new GengresForm();
                 $data = ['genres' => $item_fill->getGenres()];
                 break;
+            case Refiller::FIELD_IMAGES:
+                $form = new ImagesForm();
+                $data = ['images' => $item_fill->getImages()];
+                break;
+            case Refiller::FIELD_MANUFACTURER:
+                $form = new ManufacturerForm();
+                $data = ['manufacturer' => $item_fill->getManufacturer()];
+                break;
             case Refiller::FIELD_NAMES:
                 $form = new NamesForm();
                 $data = ['names' => $item_fill->getNames()];
                 break;
+            case Refiller::FIELD_SOURCES:
+                $form = new SourcesForm();
+                $data = ['sources' => $item_fill->getSources()];
+                break;
             case Refiller::FIELD_SUMMARY:
                 $form = new SummaryForm();
                 $data = ['summary' => $item_fill->getSummary()];
+                break;
+            case Refiller::FIELD_TRANSLATE:
+                $form = new TranslateForm();
+                $data = ['translate' => $item_fill->getTranslate()];
                 break;
             default:
                 throw $this->createNotFoundException('Field \''.$field.'\' is not supported');
