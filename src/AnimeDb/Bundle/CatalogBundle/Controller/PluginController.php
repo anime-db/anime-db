8405078e14007e6d6f1611e02bda1f5693e2e6f7
Peter Gribanov <info@peter-gribanov.ru>
2013-12-09 17:41:09 +0400

    get plugins from api and create inerface for install plugins

--- src/AnimeDb/Bundle/CatalogBundle/Controller/PluginController.php
+++ src/AnimeDb/Bundle/CatalogBundle/Controller/PluginController.php
@@ -11,6 +11,7 @@
 namespace AnimeDb\Bundle\CatalogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Guzzle\Http\Client;
 
 /**
  * Plugin
@@ -20,6 +21,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class PluginController extends Controller
 {
+    /**
+     * API server host
+     *
+     * @var string
+     */
+    const API_HOST = 'http://anime-db.org/';
+
     /**
      * Installed plugins
      *
@@ -41,8 +49,28 @@ class PluginController extends Controller
      */
     public function storeAction()
     {
+        $client = new Client(self::API_HOST);
+        /* @var $response \Guzzle\Http\Message\Response */
+        $response = $client->get('api/plugin/')->send();
+
+        if ($response->isSuccessful()) {
+            $data = json_decode($response->getBody(true), true);
+            $plugins = [];
+            foreach ($data['plugins'] as $plugin) {
+                $plugins[$plugin['name']] = $plugin;
+                $plugins[$plugin['name']]['installed'] = false;
+            }
+
+            /* @var $repository \Doctrine\ORM\EntityRepository */
+            $repository = $this->getDoctrine()->getRepository('AnimeDbAppBundle:Plugin');
+            /* @var $plugin \AnimeDb\Bundle\AppBundle\Entity\Plugin */
+            foreach ($repository->findAll() as $plugin) {
+                $plugins[$plugin->getName()]['installed'] = true;
+            }
+        }
+
         return $this->render('AnimeDbCatalogBundle:Plugin:store.html.twig', [
-            'plugins' => []
+            'plugins' => $plugins
         ]);
     }
 }
