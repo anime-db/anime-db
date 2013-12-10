6be167f0626182dc3773a8c97dfc940e4cc77dfb
Peter Gribanov <info@peter-gribanov.ru>
2013-12-10 16:23:34 +0400

    remove configuration files

--- app/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
-    new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-    new AnimeDb\Bundle\AppBundle\AnimeDbAppBundle(),
-    new AnimeDb\Bundle\CatalogBundle\AnimeDbCatalogBundle(),
-    new AnimeDb\Bundle\WorldArtFillerBundle\AnimeDbWorldArtFillerBundle(),
-    new Sensio\Bundle\BuzzBundle\SensioBuzzBundle()
-];
