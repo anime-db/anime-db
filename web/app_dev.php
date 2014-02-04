78c964cf08981cf9e6d7f8a996c3775751c1ce7c
Peter Gribanov <info@peter-gribanov.ru>
2014-02-04 18:33:34 +0400

    add caching for static in dev app

--- web/app_dev.php
+++ web/app_dev.php
@@ -24,8 +24,17 @@ $request = Request::createFromGlobals();
 
 // give static or run for dev
 if (is_file($file = __DIR__.$request->getPathInfo())) {
-    $response = new Response(file_get_contents($file));
-    $response->headers->set('Content-Type', mime_content_type($file));
+    $response = new Response();
+    $response
+        ->setPublic()
+        ->setEtag(md5_file($file))
+        ->setLastModified(new \DateTime(date('r', filemtime($file))))
+        ->headers->set('Content-Type', mime_content_type($file));
+
+    // response was not modified for this request
+    if (!$response->isNotModified($request)) {
+        $response->setContent(file_get_contents($file));
+    }
 } else {
     $response = $kernel->handle($request);
 }
