ecd38b194a8e02af73c78f69f1a8414dad1c7fea
Peter Gribanov <info@peter-gribanov.ru>
2014-02-05 10:11:08 +0400

    use apc for autoload

--- app/router.php
+++ app/router.php
@@ -61,11 +61,10 @@ if ($request->getBaseUrl() == '/app_dev.php') {
 
 
 // Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+if (extension_loaded('apc')) {
+    $loader = new ApcClassLoader('sf2', $loader);
+    $loader->register(true);
+}
 
 require_once __DIR__.'/AppKernel.php';
 require_once __DIR__.'/AppCache.php';
