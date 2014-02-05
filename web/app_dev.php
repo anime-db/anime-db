c1ed44b3a4143c7864982f61884f1e38c5d1c88d
Peter Gribanov <info@peter-gribanov.ru>
2014-02-05 10:31:30 +0400

    uncomment umask

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
-//umask(0000);
+umask(0000);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
