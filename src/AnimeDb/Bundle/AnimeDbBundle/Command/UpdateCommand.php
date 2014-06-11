afe63467e5862bf6cd2345ac707b6fdbfdefc427
Peter Gribanov <info@peter-gribanov.ru>
2014-06-11 14:02:31 +0400

    remove Guzzle

--- src/AnimeDb/Bundle/AnimeDbBundle/Command/UpdateCommand.php
+++ src/AnimeDb/Bundle/AnimeDbBundle/Command/UpdateCommand.php
@@ -14,7 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Guzzle\Http\Client;
 use Composer\Factory;
 use Composer\IO\ConsoleIO;
 use Composer\Package\Package;
