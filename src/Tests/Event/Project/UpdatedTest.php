8490dfe5e9cbce42aef050ff2eeba423c9bab8f9
Peter Gribanov <info@peter-gribanov.ru>
2014-08-12 13:25:27 +0400

    add tests for project events

---
+++ src/Tests/Event/Project/UpdatedTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * AnimeDb package
+ *
+ * @package   AnimeDb
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+ */
+
+namespace AnimeDb\Bundle\AnimeDbBundle\Tests\Event\Project;
+
+use AnimeDb\Bundle\AnimeDbBundle\Event\Project\Updated;
+
+/**
+ * Test Updated event
+ *
+ * @package AnimeDb\Bundle\AnimeDbBundle\Tests\Event\Project
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+class UpdatedTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test get package
+     */
+    public function testGetPackage()
+    {
+        $package = $this->getMockBuilder('\Composer\Package\Package')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $event = new Updated($package);
+        $this->assertInstanceOf('\Symfony\Component\EventDispatcher\Event', $event);
+        $this->assertEquals($package, $event->getPackage());
+    }
+}
