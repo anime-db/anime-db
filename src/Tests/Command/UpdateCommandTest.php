6d55661567eee89323121ea80bb8ae23a2a6d881
Peter Gribanov <info@peter-gribanov.ru>
2014-08-14 22:01:35 +0400

    add base test for update command

--- src/Tests/Command/UpdateCommandTest.php
+++ src/Tests/Command/UpdateCommandTest.php
@@ -1,32 +1,32 @@
 <?php
 /**
  * AnimeDb package
- *
- * @package   AnimeDb
- * @author    Peter Gribanov <info@peter-gribanov.ru>
- * @copyright Copyright (c) 2011, Peter Gribanov
- * @license   http://opensource.org/licenses/GPL-3.0 GPL v3
- */
+*
+* @package   AnimeDb
+* @author    Peter Gribanov <info@peter-gribanov.ru>
+* @copyright Copyright (c) 2011, Peter Gribanov
+* @license   http://opensource.org/licenses/GPL-3.0 GPL v3
+*/
 
 namespace AnimeDb\Bundle\AnimeDbBundle\Tests\Command;
 
-use AnimeDb\Bundle\AnimeDbBundle\Command\DeliverEventsCommand;
+use AnimeDb\Bundle\AnimeDbBundle\Command\UpdateCommand;
 
 /**
- * Test deliver events command
+ * Test update command
  *
  * @package AnimeDb\Bundle\AnimeDbBundle\Tests\Command
  * @author  Peter Gribanov <info@peter-gribanov.ru>
  */
-class DeliverEventsCommandTest extends \PHPUnit_Framework_TestCase
+class UpdateCommandTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * Test configure
      */
     public function testConfigure()
     {
-        $command = new DeliverEventsCommand();
-        $this->assertEquals('animedb:deliver-events', $command->getName());
+        $command = new UpdateCommand();
+        $this->assertEquals('animedb:update', $command->getName());
         $this->assertNotEmpty($command->getDescription());
     }
 
@@ -35,20 +35,15 @@ class DeliverEventsCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute()
     {
+        // TODO need write the test of execute command
+        $this->markTestSkipped('Need write the test of execute command');
+
         $input = $this->getMock('\Symfony\Component\Console\Input\InputInterface');
         $output = $this->getMock('\Symfony\Component\Console\Output\OutputInterface');
         $dispatcher = $this->getMock('\AnimeDb\Bundle\AnimeDbBundle\Event\Dispatcher');
         $container = $this->getMock('\Symfony\Component\DependencyInjection\ContainerInterface');
-        $container
-            ->expects($this->once())
-            ->method('get')
-            ->with('anime_db.event_dispatcher')
-            ->willReturn($dispatcher);
-        $dispatcher
-            ->expects($this->once())
-            ->method('shippingDeferredEvents');
 
-        $command = new DeliverEventsCommand();
+        $command = new UpdateCommand();
         $command->setContainer($container);
         $command->run($input, $output);
     }
