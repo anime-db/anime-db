4bfd88b48fc0c03af22efdb2b1fe7186379523c1
Peter Gribanov <info@peter-gribanov.ru>
2014-08-21 14:01:51 +0400

    use composer service

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -13,19 +13,14 @@ namespace AnimeDb\Bundle\AnimeDbBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Guzzle\Http\Client;
-use Composer\Factory;
-use Composer\IO\ConsoleIO;
-use Composer\Package\Package;
+use Symfony\Component\Finder\Finder;
 use AnimeDb\Bundle\AnimeDbBundle\Event\UpdateItself\StoreEvents;
 use AnimeDb\Bundle\AnimeDbBundle\Event\UpdateItself\Downloaded;
 use AnimeDb\Bundle\AnimeDbBundle\Event\UpdateItself\Updated;
 use AnimeDb\Bundle\AnimeDbBundle\Event\Dispatcher;
-use Symfony\Component\Filesystem\Filesystem;
-use Composer\Package\Loader\ArrayLoader;
-use Symfony\Component\Finder\Finder;
-use Composer\Composer;
-use Composer\Installer;
+use AnimeDb\Bundle\AnimeDbBundle\Composer\Composer;
+use Composer\Package\Package;
+use Composer\IO\ConsoleIO;
 
 /**
  * Update Application
@@ -50,9 +45,9 @@ class UpdateCommand extends ContainerAwareCommand
      * @see Symfony\Component\Console\Command.Command::execute()
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        // load composer
-        $io = new ConsoleIO($input, $output, $this->getHelperSet());
-        $composer = $this->createComposer($io);
+        /* @var $composer \AnimeDb\Bundle\AnimeDbBundle\Composer\Composer */
+        $composer = $this->getContainer()->get('anime_db.composer');
+        $composer->setIO(new ConsoleIO($input, $output, $this->getHelperSet()));
         /* @var $github \AnimeDb\Bundle\AnimeDbBundle\Client\GitHub */
         $github = $this->getContainer()->get('anime_db.client.github');
 
@@ -60,48 +55,34 @@ class UpdateCommand extends ContainerAwareCommand
         $output->writeln('Search for a new version of the application');
         $tag = $github->getLastRelease('anime-db/anime-db');
         $tag['version'] = $github->getVersionCompatible($tag['name']);
-        $current_version = $github->getVersionCompatible($composer->getPackage()->getPrettyVersion());
+        $current_version = $github->getVersionCompatible($composer->getRootPackage()->getPrettyVersion());
 
+        // update itself
         if ($tag && version_compare($tag['version'], $current_version) == 1) {
-            $this->doUpdateItself($tag, $composer, $output);
-            // reload composer
-            $composer = $this->createComposer($io);
+            $this->doUpdateItself($tag, $composer);
         } else {
             $output->writeln('<info>Application has already been updated to the latest version</info>');
         }
         unset($github, $tag, $current_version);
 
-        $this->doUpdateComposer($composer, $io);
-        $output->writeln('<info>Updating the application has been completed<info>');
-    }
-
-    /**
-     * Create new composer object
-     *
-     * @param \Composer\IO\ConsoleIO $io
-     *
-     * @return \Composer\Composer
-     */
-    protected function createComposer(ConsoleIO $io)
-    {
-        // update application components to the latest version
-        $lock_file = $this->getContainer()->getParameter('kernel.root_dir').'/../composer.lock';
-        if (file_exists($lock_file)) {
-            @unlink($lock_file);
+        // update from composer
+        if ($composer->getInstaller()->run() === 0) {
+            $output->writeln('<info>Update requirements has been completed</info>');
+        } else {
+            $output->writeln('<error>During updating dependencies error occurred</error>');
         }
-        return (new Factory())->createComposer($io);
+        $output->writeln('<info>Updating the application has been completed<info>');
     }
 
     /**
      * Do update itself
      *
      * @param array $tag
-     * @param \Composer\Composer $composer
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @param \AnimeDb\Bundle\AnimeDbBundle\Composer\Composer $composer
      */
-    protected function doUpdateItself(array $tag, Composer $composer, OutputInterface $output)
+    protected function doUpdateItself(array $tag, Composer $composer)
     {
-        $output->writeln('Discovered a new version of the application: <info>'.$tag['name'].'</info>');
+        $composer->getIO()->write('Discovered a new version of the application: <info>'.$tag['name'].'</info>');
 
         // create install package
         $package = new Package('anime-db/anime-db', $tag['version'], $tag['name']);
@@ -111,64 +92,28 @@ class UpdateCommand extends ContainerAwareCommand
 
         // download new version
         $target = sys_get_temp_dir().'/anime-db';
-        $fs = new Filesystem();
-        $fs->remove($target);
-        $composer->getDownloadManager()
-            ->getDownloaderForInstalledPackage($package)
-            ->download($package, $target);
+        $this->getContainer()->get('filesystem')->remove($target);
+        $composer->download($package, $target);
 
-        $new_package = $this->getPackage($target.'/composer.json');
+        $new_package = $composer->getPackageFromConfigFile($target.'/composer.json');
         $dispatcher = $this->getContainer()->get('event_dispatcher');
 
         // notify about downloaded
-        $dispatcher->dispatch(StoreEvents::DOWNLOADED, new Downloaded($target, $new_package, $composer->getPackage()));
+        $dispatcher->dispatch(
+            StoreEvents::DOWNLOADED,
+            new Downloaded($target, $new_package, $composer->getRootPackage())
+        );
 
         // rewriting the application files
         $this->rewriting($target);
 
         // notify about updated
         // event will be sent after the update application components
-        $dispatcher = new Dispatcher();
-        $dispatcher->dispatch(StoreEvents::UPDATED, new Updated($new_package));
+        $this->getContainer()->get('anime_db.event_dispatcher')
+            ->dispatch(StoreEvents::UPDATED, new Updated($new_package));
 
-        $output->writeln('<info>Update itself has been completed</info>');
-    }
-
-    /**
-     * Do update composer
-     *
-     * @param \Composer\Composer $composer
-     * @param \Composer\IO\ConsoleIO $io
-     */
-    protected function doUpdateComposer(Composer $composer, ConsoleIO $io)
-    {
-        if (defined('PHP_WINDOWS_VERSION_BUILD')) {
-            $composer->getDownloadManager()->setOutputProgress(false);
-        }
-        $install = Installer::create($io, $composer)
-            ->setDevMode(false)
-            ->setPreferDist(true)
-            ->setUpdate(true);
-
-        if ($install->run() === 0) {
-            $io->write('<info>Update requirements has been completed</info>');
-        } else {
-            $io->write('<error>During updating dependencies error occurred</error>');
-        }
-    }
-
-    /**
-     * Get package from config
-     *
-     * @param string $config
-     *
-     * @return \Composer\Package\RootPackage
-     */
-    protected function getPackage($config)
-    {
-        $config = json_decode(file_get_contents($config), true);
-        $loader = new ArrayLoader();
-        return $loader->load($config, 'Composer\Package\RootPackage');
+        $composer->reload();
+        $composer->getIO()->write('<info>Update itself has been completed</info>');
     }
 
     /**
@@ -178,65 +123,29 @@ class UpdateCommand extends ContainerAwareCommand
      */
     protected function rewriting($from)
     {
-        $fs = new Filesystem();
+        $fs = $this->getContainer()->get('filesystem');
         try {
-            $finder = Finder::create()
-                ->files()
-                ->ignoreUnreadableDirs()
-                ->in($this->getContainer()->getParameter('kernel.root_dir').'/../src')
-                ->in($this->getContainer()->getParameter('kernel.root_dir'))
-                ->notPath('app/DoctrineMigrations')
-                ->notPath('app/Resources')
-                ->notPath('app/bootstrap.php.cache');
-            $fs->remove($finder);
+            $fs->remove(
+                Finder::create()
+                    ->files()
+                    ->ignoreUnreadableDirs()
+                    ->in($this->getContainer()->getParameter('kernel.root_dir').'/../src')
+                    ->in($this->getContainer()->getParameter('kernel.root_dir'))
+                    ->notPath('app/DoctrineMigrations')
+                    ->notPath('app/Resources')
+                    ->notPath('app/bootstrap.php.cache')
+            );
         } catch (\Exception $e) {} // ignore errors during the removal of the old application
 
         // copy new version
-        $this->copy($from, $this->getContainer()->getParameter('kernel.root_dir').'/../');
+        $fs->mirror(
+            $from,
+            $this->getContainer()->getParameter('kernel.root_dir').'/../',
+            null,
+            ['override' => true, 'copy_on_windows' => true]
+        );
 
         // remove downloaded files
         $fs->remove($from);
-    }
-
-    /**
-     * Copy files recursive
-     *
-     * @param string $src
-     * @param string $dst
-     */
-    protected function copy($src, $dst) {
-        if (!is_array($src) && is_dir($src)) {
-            $src = new \FilesystemIterator($src);
-        }
-        $files = iterator_to_array($this->toIterator($src));
-        $files = array_reverse($files);
-        $fs = new Filesystem();
-        /* @var $file \SplFileInfo */
-        foreach ($files as $file) {
-            if (!file_exists($file) && !is_link($file)) {
-                continue;
-            }
-            if (is_dir($file) && !is_link($file)) {
-                $this->copy(new \FilesystemIterator($file), $dst.'/'.pathinfo($file, PATHINFO_BASENAME));
-            } else {
-                $fs->copy($file, $dst.'/'.pathinfo($file, PATHINFO_BASENAME), true);
-            }
-        }
-    }
-
-    /**
-     * Conver file or list of files to iterator
-     *
-     * @param mixed $files
-     *
-     * @return \Traversable
-     */
-    protected function toIterator($files)
-    {
-        if (!$files instanceof \Traversable) {
-            $files = new \ArrayObject(is_array($files) ? $files : array($files));
-        }
-
-        return $files;
     }
 }
